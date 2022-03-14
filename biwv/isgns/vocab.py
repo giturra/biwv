8ed1420a081cbd00f88ae204b2df55a55a566ea6
Gabriel Iturra <gabrieliturrab@ug.uchile.cl>
2022-03-14 15:35:37 -0300

    test vocab for isgns

--- biwv/isgns/vocab.py
+++ biwv/isgns/vocab.py
@@ -7,4 +7,17 @@ class Vocab(BaseVocab):
         super().__init__(max_size)
         self.table = defaultdict(int)
     
-    def add(sel)
+    def add(self, word):
+        if word not in self.table and not self.is_full():
+            word_index = self.current_size
+            self.table[word] = word_index
+            self.current_size += 1
+            return word_index
+        elif word in self.table:
+            return self.table[word]
+        return -1
+    
+    def __getitem__(self, word):
+        if word in self.table:
+            return self.table[word]
+        return -1
